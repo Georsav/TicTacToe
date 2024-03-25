33d38e2d6f050447e778f9e2b96012001cf5642e
Georsav <122749052+Georsav@users.noreply.github.com>
2024-03-25 12:47:21 +0200

    added players

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,5 @@
 const newGameBtn = document.querySelector(".newGameBtn");
+const resetBtn = document.querySelector(".resetBtn");
 const board = document.querySelector(".gameBoard");
 const btn1 = document.getElementById("1");
 const btn2 = document.getElementById("2");
@@ -15,17 +16,29 @@ const btnPl2 = document.getElementById("pl2");
 const winnerComp =  [[1,2,3],[4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
 const selectionX = [];
 const selectionO = [];
+const gameText = document.createElement("p");
+const resultText = document.createElement("p");
 
-console.log(gameBtns.length);
+btnDisable();
 board.dataset.symbol = "X";
 board.dataset.winner = "";
+board.dataset.player1Name = "";
+board.dataset.player2Name = "";
+
 
 newGameBtn.addEventListener('click', () => {
+    btnClear();
+    board.dataset.player1Name = document.getElementById("player1").value;
+    board.dataset.player2Name = document.getElementById("player2").value;
     document.querySelector(".playersLbls").remove();
     newGameBtn.style.margin = "8vh";
     newGameBtn.remove();
-    const gameText = document.createElement("p");
-    gameText.innerText("text");
+    gameText.innerText = "Ready to play! " + board.dataset.player1Name + " plays first";
+    document.querySelector(".playerOrder").appendChild(gameText);
+});
+
+resetBtn.addEventListener('click', () => {
+    location.reload();
 });
 
 gameBtns.forEach(button => {
@@ -36,18 +49,20 @@ gameBtns.forEach(button => {
             selectionX.push(Number(button.value));
             console.log(selectionX);
             board.dataset.symbol = "O";
+            gameText.innerText = board.dataset.player2Name + ", your turn to play.";
         } else {
             selectionO.push(Number(button.value));
             console.log(selectionO);
             board.dataset.symbol = "X";
+            gameText.innerText = board.dataset.player1Name + ", your turn to play.";
         }
         if (checkWinner(winnerComp, selectionX)) {
-            alert("Player 1 wins!");
+            resultPrgr(board.dataset.player1Name);
         } else if (checkWinner(winnerComp, selectionO)) {
-            alert("Player 2 wins!");
+            resultPrgr(board.dataset.player2Name);
         } else if (selectionO.length ===5 || selectionX.length === 5) {
             btnClear();
-            alert("It's a tie!");
+            resultPrgr("Nobody");
         }
     });
 });
@@ -76,9 +91,9 @@ function checkWinner(bigarr, smlarr) {
 function btnClear() {
     for (let i=0; i<gameBtns.length; i++) {
         gameBtns[i].innerHTML = "";
-        gameBtns.disabled = false;
+        gameBtns[i].disabled = false;
     }
-    }
+}
 
 function createPlayer(name) {
     const playerName = name;
@@ -87,4 +102,14 @@ function createPlayer(name) {
     const setScore = () => playerScore++;
     return {name, playerScore, getScore, setScore};
 }
+
+function resultPrgr(name) {
+    resultText.innerText = name + " wins!";
+    document.querySelector(".gameResult").appendChild(resultText);
+}
     
+function btnDisable() {
+    for (let i=0; i<gameBtns.length; i++) {
+        gameBtns[i].disabled = true;
+    }
+}
